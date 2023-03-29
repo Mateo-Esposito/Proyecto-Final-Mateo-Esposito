9cfb32ebffb62c936622ec121a9f1e2a09c85d57
mateo <espositosesti.mateo@gmail.com>
2023-03-28 23:18:15 -0300

    cambios carrito

--- Entrega3.js
+++ Entrega3.js
@@ -1,9 +1,21 @@
 let cartasPeliculas = document.getElementById("peliculas");
+const carrito = []
+const catalogo = []
+
+class Pelicula {
+    constructor(imagenPelicula, nombre, id){
+        this.imagenPelicula = imagenPelicula;
+        this.nombre = nombre;
+        this.id = id
+    }
+}
 
 fetch("peliculas.json")
   .then(response => response.json())
   .then(peliculas => {
     for (let creacionCartas of peliculas) {
+      let peli = new Pelicula (creacionCartas.imagenPelicula, creacionCartas.nombre, creacionCartas.id)
+      catalogo.push(peli)
       let nuevaPeliDiv = document.createElement("div");
       nuevaPeliDiv.className = "col-12 col-md-6 col-lg-4 my-3";
       nuevaPeliDiv.innerHTML =
@@ -14,7 +26,7 @@ fetch("peliculas.json")
                 <h5 class="card-title">${creacionCartas.nombre}</h5>
                 </br>
                 <div class="d-grid gap-2">
-                    <button data-id-pelicula="${creacionCartas.id}" class="btn btn-primary" type="button"> Comprar</button>
+                    <button id="${creacionCartas.id}" class="btn btn-primary botonAgregar" type="button"> Comprar</button>
                 </div>
             </div>
         </div>
@@ -24,11 +36,16 @@ fetch("peliculas.json")
     }
 
     //Evento de los botones dentro de las cards
-    let botonCompraTicket = document.getElementsByClassName("btn btn-primary");
+    let botonCompraTicket = document.getElementsByClassName("botonAgregar")
+
+    function agregarCarrito (id){
+        let peliculaSeleccionada = catalogo.find( e => e.id == id)
+        carrito.push(peliculaSeleccionada)
+        console.log(carrito)
+    }
 
     for (let i = 0; i < botonCompraTicket.length; i++) {
-      botonCompraTicket[i].addEventListener("click", function (e) {
-        //window.location.href = `index4.html?id=${e.target.dataset.idPelicula}`;
-      });
+      botonCompraTicket[i].addEventListener("click", () => agregarCarrito(botonCompraTicket[i].id))
+      
     }
-  });
+  })
